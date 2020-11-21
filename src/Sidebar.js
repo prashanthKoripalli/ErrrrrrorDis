4c8f9f9f27c9e5c659d227691615b5ab18cf6226
prashanthKoripalli <74305645+prashanthKoripalli@users.noreply.github.com>
2020-11-21 22:00:09 +0530

    codedeeee

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+    return (
+    
+        <div>className="Sidebar">
+                <h2>I am Sidebar</h2>  
+        </div>
+    );
+}
+
+export default Sidebar;
