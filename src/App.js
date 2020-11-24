882002208ca5c4ba201f6d03f1585375f876dc28
prashanthKoripalli <74305645+prashanthKoripalli@users.noreply.github.com>
2020-11-25 00:45:43 +0530

    sendd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Sidebar from "./Sidebar"
 function App() {
        return (
     <div className="app">
-      <h1>Lets build CORDD</h1>
       {/*Sidebar*/}
       <Sidebar/> 
       {/*chatbar*/}
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,71 @@
 import React from "react";
-import "./Sidebar.css";
+import  "./Sidebar.css";
+import SidebarChannel from "./SidebarChannel";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import AddIcon from '@material-ui/icons/Add';
+import  SignalCellularAltIcon  from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined"
+import CallIcon from "@material-ui/icons/Call"
+import { Avatar} from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic"
+import HeadsetIcon from "@material-ui/icons/Headset"
+import SettingsIcon from "@material-ui/icons/Settings"
 
 function Sidebar() {
     return (
-    
-        <div className="Sidebar">
-                <h2>I am Sidebar</h2>  
+<div className="sidebar_main">
+      <div className="sidebar_top">
+            <h3>Prashanth</h3>
+            <ExpandMoreIcon/>
+      </div>
+        
+        <div className="sidebar__channels">
+            <div className="sidebar__channelsHeader">
+            <div className="sidebar__header">
+                <ExpandMoreIcon/>
+                    <h4>Text Channels</h4>
+                </div>
+                <AddIcon className="sidebar__addChannel" />
+            </div>
+
+        <div className="sidebar__channelsList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
         </div>
+        </div>
+        <div classNmae="sidebar__voice">
+            <SignalCellularAltIcon
+                className="sidebar__voiceIcon"
+                // fontSize="large"
+                />
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+
+                <div className="sidebar__voiceIcons">
+                    <InfoOutlinedIcon/>
+                    <CallIcon/>
+
+                </div>
+        </div>
+        <div className="sidebar__profile">
+            <Avatar />
+            <div className="sidebar__profileInfo">
+                <h3>@ssssangha</h3>
+                <p>#thisIsMyId</p>
+            </div>
+            <div className="sidebar__profileIcons">
+                <MicIcon/>
+                <HeadsetIcon/>
+                <SettingsIcon/>
+            </div>
+        </div>
+    </div>   
+        
+        
     );
 }
 
